a483adb02f3e7e867c55e0b1a5f81ca660ce7051
Salman Sakib <salmansrizon2016@gmail.com>
2023-06-01 14:21:54 +0000

    EDA file updated

--- src/exception.py
+++ src/exception.py
@@ -22,9 +22,9 @@ class CustomException(Exception):
         return self.error_message
 
 
-if __name__ == "__main__":
-    try:
-        a = 1 / 10
-    except Exception as e:
-        logging.info("Divide by zero Error")
-        raise CustomException(e, sys)
+# if __name__ == "__main__":
+#     try:
+#         a = 1 / 10
+#     except Exception as e:
+#         logging.info("Divide by zero Error")
+#         raise CustomException(e, sys)
